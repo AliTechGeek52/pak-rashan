7b9bb2634734bc3d3bd8e39ead865a7d7de7c7f1
Nabeel Aziz <nabeel7129@gmail.com>
2020-04-05 13:56:00 -0500

    Update config.php

--- config.php
+++ config.php
@@ -5,11 +5,3 @@ $password="Fretab@23";
 $host="localhost:3306";
 $database="pakrashan_rashan";
 ?>
-
-<!-- Config for users-->
-<?php
-$username="pakrashan_admin";
-$password="Fretab@23";
-$host="localhost:3306";
-$database="pakrashan_users";
-?>
