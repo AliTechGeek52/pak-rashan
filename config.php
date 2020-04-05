0895084d300eee38c2af87dc758424544f906239
Nabeel Aziz <nabeel7129@gmail.com>
2020-04-05 13:55:09 -0500

    Update config.php
    added users config

--- config.php
+++ config.php
@@ -1,6 +1,15 @@
+<!-- Config for people-->
 <?php
 $username="pakrashan_admin";
 $password="Fretab@23";
 $host="localhost:3306";
 $database="pakrashan_rashan";
+?>
+
+<!-- Config for users-->
+<?php
+$username="pakrashan_admin";
+$password="Fretab@23";
+$host="localhost:3306";
+$database="pakrashan_users";
 ?>
