0845fe7328b8c7c72350c3f6f04f5800a4748804
Nabeel Aziz <nabeel7129@gmail.com>
2020-04-07 14:13:33 -0500

    Update req.php
    fixed insert query

--- req.php
+++ req.php
@@ -34,7 +34,7 @@ if (isset($_REQUEST['done'])) {
     $ap_number = $_REQUEST['ap_number'];
     $ap_email = $_REQUEST['ap_email'];
     $ap_password = $_REQUEST['ap_password'];
-    $sql = "INSERT INTO organizations (org_name, org_number, org_email, org_password) VALUES ('$ap_name', '$ap_number', '$ap_email', '$ap_password')";
+    $sql = "INSERT INTO organizations (org_name, org_number, org_email, org_req_password) VALUES ('$ap_name', '$ap_number', '$ap_email', '$ap_password')";
     $conn->exec($sql);
     echo "<small>data entry done</small>";
 
