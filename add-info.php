3339794d27a7c52c40e9232265ddff8d9cbb0d89
Nabeel Aziz <nabeel7129@gmail.com>
2020-04-07 13:39:38 -0500

    Update add-info.php
    updated db details

--- add-info.php
+++ add-info.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-$dsn = "mysql:host=localhost; dbname=pak";
-$db_user = "root";
-$db_password = "";
-
+$dsn = "mysql:host=localhost; dbname=pakrashan_rashan";
+$db_user = "pakrashan_admin";
+$db_password = "Fretab@23";
 
 // For connecting to database currently i am using pdo approach...
 try {
